72a56dc26053934939962d9a6365abbe533972a7
glepur <goran.lepur1@gmail.com>
2018-10-01 11:31:05 +0200

    initial commit

---
+++ server/index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const LeagueJs = require('leaguejs');
+const leagueJs = new LeagueJs(process.env.LEAGUE_API_KEY, {
+  PLATFORM_ID: process.env.LEAGUE_API_PLATFORM_ID
+});
+
+app.use(bodyParser.json());
+
+app.get('/summoner', async (req, res) => {
+  const name = req.query.name;
+  const summoner = await leagueJs.Summoner.gettingByName(name);
+  res.json({ summoner });
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.json({ error: err.message });
+});
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
