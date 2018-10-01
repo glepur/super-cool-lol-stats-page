af8fd1995d927464d3eb28f0154bad58b14c75af
glepur <goran.lepur1@gmail.com>
2018-10-01 13:29:45 +0200

    add match stats endpoint

--- server/index.js
+++ server/index.js
@@ -11,14 +11,55 @@ const leagueJs = new LeagueJs(process.env.LEAGUE_API_KEY, {
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.json({ status: 'OK' });
+app.get('/summoner', async (req, res, next) => {
+  try {
+    const name = req.query.name;
+    const summoner = await leagueJs.Summoner.gettingByName(name);
+    res.json(summoner);
+  } catch (e) {
+    next(e);
+  }
 });
 
-app.get('/summoner', async (req, res) => {
-  const name = req.query.name;
-  const summoner = await leagueJs.Summoner.gettingByName(name);
-  res.json({ summoner });
+app.get('/summoner/:accountId/matches', async (req, res) => {
+  try {
+    const { accountId } = req.params;
+    const { matches } = await leagueJs.Match.gettingListByAccount(
+      accountId,
+      null,
+      {
+        beginIndex: 0,
+        endIndex: 10
+      }
+    );
+    if (matches && matches.length) {
+      res.json(matches);
+    } else {
+      res.status(404).json({ message: 'No matches found' });
+    }
+  } catch (e) {
+    next(e);
+  }
+});
+
+app.get('/summoner/:accountId/matches/:matchId', async (req, res) => {
+  const { accountId, matchId } = req.params;
+  const match = await leagueJs.Match.gettingById(matchId);
+  if (match && match.gameId) {
+    const { participantId } = match.participantIdentities.find(
+      identity => identity.player.currentAccountId === +accountId
+    );
+    const participant = match.participants.find(
+      participant => participant.participantId === +participantId
+    );
+    res.json(participant.stats);
+  } else {
+    res.status(404).json({ message: 'Match not found' });
+  }
+});
+
+app.get('/', (req, res) => {
+  res.json({ status: 'OK' });
 });
 
 app.use((err, req, res, next) => {
