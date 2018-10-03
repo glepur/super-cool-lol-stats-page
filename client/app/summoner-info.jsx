9dadb01072ce819a5b6efbf86f8c42b8e037c400
glepur <goran.lepur1@gmail.com>
2018-10-03 14:04:01 +0200

    show not found text if summoner not found

--- client/app/summoner-info.jsx
+++ client/app/summoner-info.jsx
@@ -2,20 +2,32 @@ import React, { Component } from 'react';
 
 export default function SummonerInfo(props) {
   if (props.summoner) {
-    return (
-      <div className="row mb-2">
-        <img
-          width="100"
-          height="100"
-          src={getProfileImageUrl(props.dataDragonUrl, props.summoner)}
-          alt="Profile Image"
-        />
-        <h2 className="ml-2">{props.summoner.name}</h2>
-      </div>
-    );
+    return props.summoner.name ? summonerFound(props) : summonerNotFound();
   } else {
     return null;
   }
+}
+
+function summonerFound(props) {
+  return (
+    <div className="row mb-2">
+      <img
+        width="100"
+        height="100"
+        src={getProfileImageUrl(props.dataDragonUrl, props.summoner)}
+        alt="Profile Image"
+      />
+      <h2 className="ml-2">{props.summoner.name}</h2>
+    </div>
+  );
+}
+
+function summonerNotFound() {
+  return (
+    <div className="row mb-2">
+      <h2 className="ml-2">Summoner not found</h2>
+    </div>
+  );
 }
 
 function getProfileImageUrl(dataDragonUrl, summoner) {
